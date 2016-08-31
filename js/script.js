5f9704d1cc6b96124238f1ff5f519cd252aa6f4f
dylannegri <negrm581@newschool.edu>
2016-08-30 23:59:52 -0400

    second commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,70 @@
 var slideIndex = 1;
 
+
+/*
+ * Function switchSlide(): Switching images while clicking arrows.
+ */
 function switchSlide(n) {
-	showSlide(slideIndex += n);
+    showSlide(slideIndex += n);
 }
+
+/*
+ * Function currentSlide(): Switching images while clicking dots.
+ */
 function currentSlide(n) {
-	showSlide(slideIndex = n)
+    showSlide(slideIndex = n);
 }
+
 
 function showSlide(n) {
 
-	var slideArray = document.getElementsByClassName('slides');
-	var slideArray = document.getElementsByClassName('myDot');
+    var slidesArray
+        = document.getElementsByClassName('slides');
+    var dotsArray
+        = document.getElementsByClassName('myDot');
 
-	if (n > slideArray.length) {
-		slideIndex = 1;
-	}
-	if (n < 1) {
-		slideIndex = slideArray.length;
-	}
-	for (var i = 0; i < slideArray.length; i++) {
-		slideArray[i].className = slideArray[i].className.replace(' fadeImage', '');
-	}
-	slideArray[slideIndex-1].className += ' fadeImage';
+    /*
+     * When it goes over the slideIndex, it returns back the first one.
+     */
+    if (n > slidesArray.length) {
+        slideIndex = 1;
+    }
 
-	for (var i = 0; i < dotsArray.length; i++) {
-		dotsArray[i].className = dotsArray[i].className.replace(' dot-filled', '');
-	}
-		slideArray[slideIndex-1].className += ' dot-filled';
+    /*
+     * When it is lesser than the slideIndex, it goes to the last one.
+     */
+    if (n < 1) {
+        slideIndex = slidesArray.length;
+    }
+
+    /*
+     * Clear all the fadeImage class
+     */
+    for (var i = 0; i < slidesArray.length; i++) {
+        slidesArray[i].className
+        = slidesArray[i].className.replace(" fadeImage", "");
+    }
+    /*
+     * Add fadeImage to show the Image that should appear.
+     */
+    slidesArray[slideIndex-1].className += " fadeImage";
+
+
+    /*
+     * Clear all the dot-filled class
+     */
+    for (var i = 0; i < dotsArray.length; i++) {
+        dotsArray[i].className = dotsArray[i].className.replace(" dot-filled", "");
+    }
+    /*
+     * Fill the dot for the image according to the slideIndex.
+     * i.e, If it is Image02.jpg, then fill the second dot's background.
+     */
+    dotsArray[slideIndex-1].className += " dot-filled";
 }
+
+
+
 window.setInterval(
    function(){
    switchSlide(1)
